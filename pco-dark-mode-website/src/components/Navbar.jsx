f3b6b3f4b9a6c60d6fda3f85465bf501b77a52c4
sean <sean@sscott.dev>
2023-07-19 11:40:23 -0400

    start

---
+++ pco-dark-mode-website/src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import styles from "../styles/Navbar.module.css"
+
+export default function Navbar() {
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static" sx={{ backgroundColor: "#1f2124" }}>
+                <Toolbar>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        PCO Dark Mode
+                    </Typography>
+                    <Button
+                        sx={{
+                            boxShadow: 'inset 0px 0px 0px 1px #676767',
+                        }}
+                        color="inherit"
+                    >
+                        Download (Chrome Web Store)
+                    </Button>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
+}
---
+++ pco-dark-mode-website/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import styles from '../styles/Home.module.css';
+
+function Home() {
+    return (
+        <div className={styles.mainText}>
+            <h1>Tired of no</h1> <h1 className={styles.darkword}>dark</h1> <h1>mode on PCO?</h1>
+        </div>
+    );
+}
+
+export default Home;
